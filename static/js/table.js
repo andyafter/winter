646ca74b4c0ac1e4da5108f3dd083fc762ce6192
andyafter <andyafter@gmail.com>
2015-04-29 18:21:28 +0800

    started with canvas

---
+++ static/js/table.js
@@ -0,0 +1,7 @@
+var canvas = document.getElementById('pokertable');
+var context = canvas.getContext('2d');
+
+context.beginPath();
+context.moveTo(100, 150);
+context.lineTo(450, 50);
+context.stroke();
--- winter.py
+++ winter.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('index.html')
+@app.route("/new")
+def new():
+    return "new"
+
+@app.route("/user/<s>")
+def pstr(s):
+    return s
+
+@app.route("/table")
+def pokerTable():
+    return render_template('table.html')
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
