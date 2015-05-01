d908cd64463f1386efc71906c1c06cfc7676ed40
andyafter <andyafter@gmail.com>
2015-05-01 13:51:03 +0800

    nn

--- static/js/table.js
+++ static/js/table.js
@@ -8,7 +8,9 @@ function init(){
     circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
     //circle.x = 100;
     //circle.y = 100;
-    stage.addChild(circle);
+    var graphics = new createjs.Graphics().beginFill("#ff0000").drawRoundRectComplex(250,200,50,50,5,5,5,5);
+    var shape = new createjs.Shape(graphics);
+    stage.addChild(shape);
     stage.update();
 }
 
